b7acfb557363ec3d6d155c73077498fcfda2a8ad
duhdanube <Danny.B.Lim@gmail.com>
2016-08-08 10:47:13 -0700

    Fix error attempt 2

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(process.env.JAWSDB_URL || {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  database: process.env.MYSQL_DATABASE
 });
 
 db.connect();
