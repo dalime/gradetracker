b66d5bb42663f08bd4425a71a25c80f74c6bda49
duhdanube <Danny.B.Lim@gmail.com>
2016-08-08 12:28:59 -0700

    Fix grading

--- routes/assignments.js
+++ routes/assignments.js
@@ -13,11 +13,11 @@ router.get('/', (req, res) => {
         totalScores += assignments[i].score;
         if (assignments[i].score / assignments[i].total >= 0.9) {
           assignments[i]["grade"] = "A";
-        } else if (assignments[i].score / assignments[i].total >= 0.8 || assignments[i].score / assignments[i].total < 0.9) {
+        } else if (assignments[i].score / assignments[i].total >= 0.8 && assignments[i].score / assignments[i].total < 0.9) {
           assignments[i]["grade"] = "B";
-        } else if (assignments[i].score / assignments[i].total >= 0.7 || assignments[i].score / assignments[i].total < 0.8) {
+        } else if (assignments[i].score / assignments[i].total >= 0.7 && assignments[i].score / assignments[i].total < 0.8) {
           assignments[i]["grade"] = "C";
-        } else if (assignments[i].score / assignments[i].total >= 0.6 || assignments[i].score / assignments[i].total < 0.7) {
+        } else if (assignments[i].score / assignments[i].total >= 0.6 && assignments[i].score / assignments[i].total < 0.7) {
           assignments[i]["grade"] = "D";
         } else {
           assignments[i]["grade"] = "F";
